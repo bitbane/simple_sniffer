a4c972218888c3bc5dc9973ed2ee422a5fa5527a
Tim Brom <thb@skadi>
2016-04-05 12:22:57 -0400

    Looks like I am now writing a valid PCAP file too

--- simple_sniffer.c
+++ simple_sniffer.c
@@ -5,6 +5,27 @@
 #include <arpa/inet.h>
 #include <netinet/if_ether.h> 
 #include <unistd.h>
+#include <sys/time.h>
+#include <stdint.h>
+
+/** PCAP file header */
+typedef struct pcap_hdr_s {
+    uint32_t magic_number;   /* magic number */
+    uint16_t version_major;  /* major version number */
+    uint16_t version_minor;  /* minor version number */
+    int32_t  thiszone;       /* GMT to local correction */
+    uint32_t sigfigs;        /* accuracy of timestamps */
+    uint32_t snaplen;        /* max length of captured packets, in octets */
+    uint32_t network;        /* data link type */
+} pcap_hdr_t;
+
+/** PCAP packet header */
+typedef struct pcaprec_hdr_s {
+    uint32_t ts_sec;         /* timestamp seconds */
+    uint32_t ts_usec;        /* timestamp microseconds */
+    uint32_t incl_len;       /* number of octets of packet saved in file */
+    uint32_t orig_len;       /* actual length of packet */
+} pcaprec_hdr_t;
 
 const int MAX_SIZE = 16384;
 
@@ -28,6 +49,10 @@ int main(int argc, char** argv)
         exit(-1);
     }
 
+    // Set up the PCAP file header (Magic Number, Major Version, Minor Version, UTC Timezone, Accuracy, Snaplen, Link Type Ethernet)
+    pcap_hdr_t pcap_hdr = {0xa1b2c3d4, 2, 4, 0, 0, MAX_SIZE, 1};
+    fwrite((char*)&pcap_hdr, 1, sizeof(pcap_hdr_t), outfile);
+
     int sock_raw = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
     if(sock_raw < 0)
     {
@@ -44,6 +69,14 @@ int main(int argc, char** argv)
             printf("Recvfrom error , failed to get packets\n");
             exit(-1);
         }
+        pcaprec_hdr_t packet_hdr;
+        struct timeval tv;
+        gettimeofday(&tv, NULL);
+        packet_hdr.incl_len = data_size;
+        packet_hdr.orig_len = data_size; // FIXME: This could be wrong if packet length was > MAX_SIZE
+        packet_hdr.ts_sec = tv.tv_sec;
+        packet_hdr.ts_usec = tv.tv_usec;
+        fwrite((char*)&packet_hdr, 1, sizeof(pcaprec_hdr_t), outfile);
         fwrite(buffer, 1, data_size, outfile);
     }
     fclose(outfile);
